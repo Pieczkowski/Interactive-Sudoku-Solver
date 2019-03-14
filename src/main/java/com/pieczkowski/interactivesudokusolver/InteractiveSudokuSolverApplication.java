90e64cb7839127e92f7402c362cdb04c7fdee097
Pieczkowski <pieczkowski.t@gmail.com>
2019-03-14 14:55:58 +0100

    Add Spring Boot project structure

---
+++ src/main/java/com/pieczkowski/interactivesudokusolver/InteractiveSudokuSolverApplication.java
@@ -0,0 +1,13 @@
+package com.pieczkowski.interactivesudokusolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveSudokuSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveSudokuSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pieczkowski/interactivesudokusolver/InteractiveSudokuSolverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pieczkowski.interactivesudokusolver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InteractiveSudokuSolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
