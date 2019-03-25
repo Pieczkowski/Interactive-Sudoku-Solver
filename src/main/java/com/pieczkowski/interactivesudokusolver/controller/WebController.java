a4d020fdd9b35d3834b4a31e390f8ccaaf4bf9eb
Pieczkowski <pieczkowski.t@gmail.com>
2019-03-25 05:03:53 +0100

    Add WebController

---
+++ src/main/java/com/pieczkowski/interactivesudokusolver/controller/WebController.java
@@ -0,0 +1,28 @@
+package com.pieczkowski.interactivesudokusolver.controller;
+
+import com.pieczkowski.interactivesudokusolver.model.Sudoku;
+import com.pieczkowski.interactivesudokusolver.solver.Solver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class WebController {
+    private Solver solver;
+
+    @Autowired
+    public WebController(Solver solver){
+        this.solver = solver;
+    }
+
+    @PostMapping(value = "/solve", consumes = "application/json")
+    @ResponseBody
+    public Sudoku solve(@RequestBody Sudoku form){
+        boolean isSolved = solver.solve(form);
+        if (isSolved) {
+            form.markAsSolved();
+        }
+        return form;
+    }
+
+}
